83b8ad851a21997f20033cdbcd6bef4fd557a849
sheikhmubashi <57352014+sheikhmubashi@users.noreply.github.com>
2020-06-20 12:45:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,151 @@
+//---------Chepter 1 Question 1---------//
+
+//alert("Welcome to JS Happy codding");
+
+//---------Chepter 1 Question 2---------//
+
+//alert("Error! Please enter a valid password.");
+
+//---------Chepter 1 Question 3---------//
+
+//alert("Welcome to JS Land\nHeppy Codding");
+
+//---------Chepter 1 Question 4---------//
+
+//alert("Welcome to JS Land...");
+//alert("Happy Codding\nPrevent this page to creating additional dialogs");
+
+//---------Chepter 1 Question 5---------//
+
+//console.log("Hello....I can run JS trough my browser console");
+
+//---------Chepter 1 Question 6 (Check HTML & CSS)---------//
+//---------Chepter 1 Question 7 (Check HTML & CSS)---------//
+
+//---------Chepter 2 Question 1---------//
+
+//var username;
+
+//---------Chepter 2 Question 2---------//
+
+//var myName = "Sheikh Mubashir";
+
+//---------Chepter 2 Question 3---------//
+
+//var message = "“Hello World”";
+//alert(message);
+
+//---------Chepter 2 Question 4---------//
+
+// var name = "Jhone Doe, ";
+// alert(name);
+// var age = "15 Years OLD ";
+// alert(age);
+// var education = "Certified Mobile Application Developmnet";
+// alert(education);
+
+//---------Chepter 2 Question 5---------//
+
+//alert("pizza \n pizz \n piz \n pi \n p");
+
+//---------Chepter 2 Question 6---------//
+
+// var email = "smubashir655@gmail.com";
+// alert("My email address is " + email);
+
+//---------Chepter 2 Question 7---------//
+
+// var book =  "“A smarter way to learn JavaScript”";
+// alert(book);
+
+//---------Chepter 2 Question 8---------//
+
+//document.write("Yah! I can write HTML content trhough javaScript");
+
+//---------Chepter 2 Question 9---------//
+
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(design);
+
+//---------Chepter 3 Question 1---------//
+
+// var age = "Im 15 years old";
+// alert(age);
+
+//---------Chepter 3 Question 2---------//
+
+// var visit = "“You have visited this site 14 times”.";
+// alert(visit);
+
+//---------Chepter 3 Question 3---------//
+
+// document.write("My birth year is 1997 <br> Data type of my ddeclared variable is number");
+
+//---------Chepter 4 Question 1---------//
+
+// var variable1 = "Hello World!";
+// var variable2 = "Hello";
+// var variable3 = 20;
+
+//---------Chepter 4 Question 2---------//
+
+// var ligal = "_ $ h# h8 ";
+// var iligal = "2 6g @ ! :";
+
+//---------Chepter 4 Question 3---------//
+
+// document.write("Rules for naming JS variables <br>  <br> Variable names can only contain: number, $ & _ For example $my_1stVariable <br> Variables must begin with a $ or _ For example $name, _name or name  <br>  Variable names are case sensitive <br> Variable names should not be JS keywords ");
+
+//---------Chepter 5 Question 1---------//
+
+//  var sum = 2 + 6;
+//  alert(sum);
+
+//---------Chepter 5 Question 2---------//
+
+// var sum = 2 - 6;
+// alert(sum);
+
+// var sum = 2 * 6;
+//  alert(sum);
+
+//  var sum = 2 / 6;
+//  alert(sum);
+
+//  var sum = 2 % 6;
+//  alert(sum);
+
+//---------Chepter 5 Question 4---------//
+
+// var ticket = 600 * 5;
+// document.write("Total cost to buy 5 tickets for a movie is PKR" + ticket);
+
+//---------Chepter 5 Question 5---------//
+
+// document.write("Table of 5 <br> 5 * 1 = 5 <br> 5 * 2 = 10 <br> 5 * 3 = 15 <br> 5 * 4 = 20 <br> 5 * 5 = 25 <br> 5 * 6 = 30 <br> 5 * 7 = 35 <br> 5 * 8 = 40 <br> 5 * 9 = 45 <br>  5 * 10 = 50");
+
+//---------Chepter 5 Question 7---------//
+
+// document.write("Shopping Cart <br> <br> Price of item 650 <br> Price of item 200 <br> quantity of item 1 <br> Quantity of item 2 <br> Shipping charges 200 <br> <br> Total 1050");
+
+//---------Chepter 5 Question 8---------//
+
+// document.write("Mark Sheet  <br> <br> Total Marks: 980 <br> mark obtained: 804 <br> Percentage: 82% ");
+
+//---------Chepter 21-25 Question 1---------//
+
+// var firstName = 'Mubashir';
+// var lastName = 'Ejaz';
+// var fullName = firstName + ' ' + lastName;
+// alert(fullName + ' ' + "Welcome to JS World");
+
+
+
+
+
+
+
+
+
+
+
